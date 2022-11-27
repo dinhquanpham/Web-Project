cc053dc238d8dff8ee4fbfcb500a23aff42a61cf
Vũ Minh Sang <80307614+sangvm@users.noreply.github.com>
2022-11-27 20:34:38 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,12 +4,16 @@ const model = require('./database/index');
 const router = require('./routers/MainRouters');
 const cookieParser = require('cookie-parser')
 const app = express();
+const cors = require('cors')
 const port = 3030;
 
 const init = async () => {
   //database
   model.loadModels();
   await sequelize.sync();
+
+  //cors
+  app.use(cors());
 
   //middleware
   app.use(express.json());
