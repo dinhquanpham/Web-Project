0a1d38743f80c26b903bad166cee708e7b403fbb
Pham Dinh Quan <78460153+dinhquanpham@users.noreply.github.com>
2022-12-08 22:14:12 +0700

    update

--- backend/controllers/OrderController.js
+++ backend/controllers/OrderController.js
@@ -21,8 +21,7 @@ const getOrderByUser = async (req, res) => {
 const addOrder = async (req, res) => {
     let data = req.body;
 
-
-
+    
     let orderData = data[0];
     let productData = data;
 
@@ -30,12 +29,16 @@ const addOrder = async (req, res) => {
     data[0] = order;
     const product = await orderDetailService.addOrderDetail(productData);
 
-    const result = {
-        order: order,
-        product: product
+    if (data[0].paymentId == 2) {
+        res.send("Đơn hàng của bạn đã được tiếp nhận")
     }
-
-    res.send(result);
+    else {
+        let amount = data[0].paidAmount;
+        let url = process.env.PAYMENT_QR_URL + amount + '&accountName=' + process.env.PAYMENT_QR_ACCOUNTNAME;
+        console.log(url);
+        res.redirect(url);
+    }
+    
 }
 
 const updateOrder = async (req, res) => {
